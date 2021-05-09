7ef82fc140d0d48d35c9e28640c9123471faf92e
Tristen Trias <tntrias2@gmail.com>
2021-05-08 22:33:56 -0700

    Gridshot initial commit Added randomized functions

--- ClickTrainer.pro
+++ ClickTrainer.pro
@@ -10,12 +10,10 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    clickablelabel.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    clickablelabel.h \
     mainwindow.h
 
 FORMS += \
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "clickablelabel.h"
 #include <QRandomGenerator>
 #include <QCoreApplication>
+#include <QIcon>
+
 #include <iostream>
+#include <vector>
+#include <random>
+#include <string>
 
 using namespace std;
 
@@ -11,31 +15,11 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    srand(time(0));
     ui->setupUi(this);
 
-    QRandomGenerator *r = QRandomGenerator::global();
-    int rand = r->bounded(0, 16);
-    const QString a = QString::number(rand);
+    //ui->reacButton->setGeometry();
 
-    updateLabel(a);
-
-    QPixmap pix("F:/Libraries/Documents/UCM/CSE/CSE 165/QT Projects/ClickTrainer/assets/circle.png");
-    ui->G1->setPixmap(pix);
-    ui->G2->setPixmap(pix);
-    ui->G3->setPixmap(pix);
-    ui->G4->setPixmap(pix);
-    ui->G5->setPixmap(pix);
-    ui->G6->setPixmap(pix);
-    ui->G7->setPixmap(pix);
-    ui->G8->setPixmap(pix);
-    ui->G9->setPixmap(pix);
-    ui->G10->setPixmap(pix);
-    ui->G11->setPixmap(pix);
-    ui->G12->setPixmap(pix);
-    ui->G13->setPixmap(pix);
-    ui->G14->setPixmap(pix);
-    ui->G15->setPixmap(pix);
-    ui->G16->setPixmap(pix);
 }
 
 MainWindow::~MainWindow()
@@ -43,8 +27,109 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::startTimer()
+{
+
+}
+
+void MainWindow::buttonClicked() {
+    QPushButton *btn = qobject_cast<QPushButton *>(sender());
+    QPushButton *temp;
+
+    if (btn->isVisible()) {
+        updateScore();
+        temp = buttonVisible(randButton());
+        btn->setVisible(false);
+        temp->setVisible(true);
+    }
+}
+
+void MainWindow::updateScore(int n) {
+    score = n;
+    QString temp = QString::number(score);
+    ui->score->setText("Score: " + temp);
+}
+
+void MainWindow::updateScore() {
+    score += 1;
+    QString temp = QString::number(score);
+    ui->score->setText("Score: " + temp);
+}
+
+
+
+QPushButton* MainWindow::buttonVisible(QPushButton* btn) {
+    if (!btn->isVisible())
+        return btn;
+    else {
+        btn = randButton();
+        return buttonVisible(btn);
+    }
+}
+
+void MainWindow::setGrid() {
+    QLayoutItem* item;
+    for (int i = 0; i < ui->gridLayout->rowCount(); i++) {
+        for (int j = 0; j < ui->gridLayout->columnCount(); j++) {
+            item = ui->gridLayout->itemAtPosition(i, j);
+            if (item->widget()) {
+                QPushButton* btn = qobject_cast<QPushButton *>(item->widget());
+                connect(btn, &QPushButton::clicked, this, &MainWindow::buttonClicked);
+                setButton(btn);
+                btn->setVisible(false);
+            }
+        }
+    }
+    updateScore(0);
+}
+
+QPushButton* MainWindow::randButton() {
+    int x, y;
+    x = randNumber4();
+    y = randNumber4();
+
+    QLayoutItem *temp = ui->gridLayout->itemAtPosition(x, y);
+    QPushButton *btn = qobject_cast<QPushButton *>(temp->widget());
+
+    return btn;
+}
+
+void MainWindow::gridGame() {
+    QPushButton* btn;
+
+    setGrid();
+
+    for (int i = 0; i < 3; i++) {
+        btn = randButton();
+        btn->setVisible(true);
+    }
+
+}
+
+int MainWindow::randNumber16() {
+    return rand() % 15;
+}
+
+int MainWindow::randNumber4() {
+    return rand() % 3;
+}
+
+void MainWindow::setReac() {
+    setButton(ui->reacButton);
+}
+
+void MainWindow::setButton(QPushButton* button) {
+    const QPixmap pix("F:/Libraries/Documents/UCM/CSE/CSE 165/QT Projects/ClickTrainer/assets/circle.png");
+    QIcon icon(pix);
+
+    button->setFixedSize(icon.actualSize(icon.availableSizes().first()));
+    button->setText("");
+    button->setIcon(icon);
+    button->setIconSize(icon.availableSizes().first());
+}
+
 void MainWindow::on_goYellow_released() {
-    ui->stackedWidget->setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 void MainWindow::on_goBlue_released()
@@ -52,22 +137,19 @@ void MainWindow::on_goBlue_released()
     ui->stackedWidget->setCurrentIndex(0);
 }
 
-
-void MainWindow::on_G1_linkActivated(const QString &link)
+void MainWindow::on_grid_released()
 {
-
+    ui->stackedWidget->setCurrentIndex(2);
+    gridGame();
 }
 
-void MainWindow::on_G3_2_linkActivated(const QString &link){
-    ui->G3->setVisible(true);
+void MainWindow::on_reac_released()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+    setReac();
 }
 
-void MainWindow::on_asd_windowIconTextChanged(const QString &iconText)
+void MainWindow::updateLabel(const QString text, QLabel* label)
 {
-
-}
-
-void MainWindow::updateLabel(const QString text)
-{
-    ui->asd->setText(text);
+    label->setText(text);
 }
